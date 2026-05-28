9c6218541f13b80ccf82352bc04cff31de5cdcd2
kyooonnnggg <gyoeunjung23@sookmyung.ac.kr>
2026-05-29 06:58:50 +0900

    setting: 이미지 에셋 세팅

---
+++ SOPT-SwiftUI/SOPT-SwiftUI/SOPT_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOPT_SwiftUIApp.swift
+//  SOPT-SwiftUI
+//
+//  Created by 정교은 on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct SOPT_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
